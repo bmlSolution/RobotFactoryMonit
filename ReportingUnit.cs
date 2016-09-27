85bcf04605b3ed4a111a4a7d9e429a7037e9f45f
brueara <bruno.marchand-lourette@c17e.com>
2016-09-27 11:06:01 +0200

    Add files via upload
    Last Import

---
+++ ReportingUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Reporting
+{
+    public delegate void UnitStatusChanged(object sender, EventArgs e);
+
+    public abstract class ReportingUnit
+    {
+
+
+        public virtual void OnStatusChanged(StatusChangedEventArgs newStatus)
+        {
+            UnitStatusChanged(this, newStatus);
+        }
+    }
+
+    public class StatusChangedEventArgs : EventArgs
+    {
+        string NewStatus;
+
+
+    }
+}
+
